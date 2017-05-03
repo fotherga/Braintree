9b69fac7476fb492ae39aa36a8819893f439579c
Adam Fothergill <adam.fothergill@mendix.com>
2017-05-03 17:13:00 -0400

    Initial updates to js/xml/css/html

--- src/Braintree/widget/Braintree.js
+++ src/Braintree/widget/Braintree.js
@@ -47,10 +47,10 @@ define([
         templateString: widgetTemplate,
 
         // DOM elements
-        inputNodes: null,
-        colorSelectNode: null,
-        colorInputNode: null,
-        infoTextNode: null,
+        // inputNodes: null,
+        // colorSelectNode: null,
+        // colorInputNode: null,
+        // infoTextNode: null,
 
         // Parameters configured in the Modeler.
         mfToExecute: "",
@@ -122,20 +122,20 @@ define([
         // Attach events to HTML dom elements
         _setupEvents: function () {
             logger.debug(this.id + "._setupEvents");
-            this.connect(this.colorSelectNode, "change", function (e) {
-                // Function from mendix object to set an attribute.
-                this._contextObj.set(this.backgroundColor, this.colorSelectNode.value);
-            });
+            // this.connect(this.colorSelectNode, "change", function (e) {
+            //     // Function from mendix object to set an attribute.
+            //     this._contextObj.set(this.backgroundColor, this.colorSelectNode.value);
+            // });
 
-            this.connect(this.infoTextNode, "click", function (e) {
-                // Only on mobile stop event bubbling!
-                this._stopBubblingEventOnMobile(e);
+            // this.connect(this.infoTextNode, "click", function (e) {
+            //     // Only on mobile stop event bubbling!
+            //     this._stopBubblingEventOnMobile(e);
 
-                // If a microflow has been set execute the microflow on a click.
-                if (this.mfToExecute !== "") {
-                    this._execMf(this.mfToExecute, this._contextObj.getGuid());
-                }
-            });
+            //     // If a microflow has been set execute the microflow on a click.
+            //     if (this.mfToExecute !== "") {
+            //         this._execMf(this.mfToExecute, this._contextObj.getGuid());
+            //     }
+            // });
         },
 
         _execMf: function (mf, guid, cb) {
@@ -161,28 +161,28 @@ define([
         // Rerender the interface.
         _updateRendering: function (callback) {
             logger.debug(this.id + "._updateRendering");
-            this.colorSelectNode.disabled = this._readOnly;
-            this.colorInputNode.disabled = this._readOnly;
+            // this.colorSelectNode.disabled = this._readOnly;
+            // this.colorInputNode.disabled = this._readOnly;
 
-            if (this._contextObj !== null) {
-                dojoStyle.set(this.domNode, "display", "block");
+            // if (this._contextObj !== null) {
+            //     dojoStyle.set(this.domNode, "display", "block");
 
-                var colorValue = this._contextObj.get(this.backgroundColor);
+            //     var colorValue = this._contextObj.get(this.backgroundColor);
 
-                this.colorInputNode.value = colorValue;
-                this.colorSelectNode.value = colorValue;
+            //     this.colorInputNode.value = colorValue;
+            //     this.colorSelectNode.value = colorValue;
 
-                dojoHtml.set(this.infoTextNode, this.messageString);
-                dojoStyle.set(this.infoTextNode, "background-color", colorValue);
-            } else {
-                dojoStyle.set(this.domNode, "display", "none");
-            }
+            //     dojoHtml.set(this.infoTextNode, this.messageString);
+            //     dojoStyle.set(this.infoTextNode, "background-color", colorValue);
+            // } else {
+            //     dojoStyle.set(this.domNode, "display", "none");
+            // }
 
-            // Important to clear all validations!
-            this._clearValidations();
+            // // Important to clear all validations!
+            // this._clearValidations();
 
-            // The callback, coming from update, needs to be executed, to let the page know it finished rendering
-            this._executeCallback(callback, "_updateRendering");
+            // // The callback, coming from update, needs to be executed, to let the page know it finished rendering
+            // this._executeCallback(callback, "_updateRendering");
         },
 
         // Handle validations.
